c5bdd8409a5f1f053c41e722b95793d5cb3e9e8f
Tristan Beasley <twbeasley13@gmail.com>
2018-10-08 13:16:38 -0400

    added .rb files for each piece

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,9 +1,2 @@
 class ChessPiece < ApplicationRecord
 end
-
-
-class Knight < ChessPiece; end
-class Bishop < ChessPiece; end
-class Rook < ChessPiece; end
-class Queen < ChessPiece; end
-class King < ChessPiece; end
