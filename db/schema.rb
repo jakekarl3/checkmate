d2b52f972c492cf856907e3339f05ec87bc09fef
lisarics <lisa.richardson@gmail.com>
2018-10-13 15:15:50 +0000

    bugfix: renamed columns for position_x_position_y

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008195748) do
+ActiveRecord::Schema.define(version: 20181013151031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,11 @@ ActiveRecord::Schema.define(version: 20181008195748) do
     t.string   "name"
     t.string   "type"
     t.string   "color"
-    t.integer  "postion_x"
-    t.integer  "postion_y"
+    t.integer  "position_x"
+    t.integer  "position_y"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "game_id"
   end
 
   create_table "games", force: :cascade do |t|
